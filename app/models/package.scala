b8aa0c8f0f8bbd9b453071cd3a3bdee4e79ab288
Matthew Snaith <fault_line@hotmail.co.uk>
2014-11-11 08:50:30 +0000

    us1660 PrivateKeeperDetails cache key to be removed on BeforeYouStart present

--- app/models/package.scala
+++ app/models/package.scala
@@ -1,11 +1,15 @@
 import models.VehicleLookupFormModel.VehicleLookupFormModelCacheKey
+import models.PrivateKeeperDetailsFormModel.PrivateKeeperDetailsCacheKey
 
 package object models {
   final val HelpCacheKey = "help"
   final val SeenCookieMessageCacheKey = "seen_cookie_message"
 
   // The full set of cache keys. These are removed at the start of the process in the "before_you_start" page
-  final val AllCacheKeys = Set(VehicleLookupFormModelCacheKey)
+  final val AllCacheKeys = Set(
+    VehicleLookupFormModelCacheKey,
+    PrivateKeeperDetailsCacheKey
+  )
 
   // Set of cookies related to all data entered for a private keeper
   final val PrivateKeeperDetailsCacheKeys = Set("fixMe")
