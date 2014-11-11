905f3a2e68f00fadadacf34fdd8a2d9b4efb8a80
Matthew Snaith <fault_line@hotmail.co.uk>
2014-11-11 10:50:46 +0000

    all vehicle and keeper cache keys to be removed on BeforeYouStart present, test added

--- app/models/package.scala
+++ app/models/package.scala
@@ -1,20 +1,21 @@
 import models.VehicleLookupFormModel.VehicleLookupFormModelCacheKey
 import models.PrivateKeeperDetailsFormModel.PrivateKeeperDetailsCacheKey
+import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel.VehicleAndKeeperLookupDetailsCacheKey
 
 package object models {
   final val HelpCacheKey = "help"
   final val SeenCookieMessageCacheKey = "seen_cookie_message"
 
-  // The full set of cache keys. These are removed at the start of the process in the "before_you_start" page
-  final val AllCacheKeys = Set(
+  final val VehicleDetailsCacheKeys = Set(
     VehicleLookupFormModelCacheKey,
-    PrivateKeeperDetailsCacheKey
+    VehicleAndKeeperLookupDetailsCacheKey
   )
 
-  // Set of cookies related to all data entered for a private keeper
-  final val PrivateKeeperDetailsCacheKeys = Set("fixMe")
+  final val PrivateKeeperDetailsCacheKeys = Set(PrivateKeeperDetailsCacheKey)
 
-  // Set of cookies related to all data entered for a business keeper
-  final val BusinessKeeperDetailsCacheKeys = Set("fixMe")
+  final val BusinessKeeperDetailsCacheKeys = Set("enter business keeper cache keys here")
 
+  final val AllCacheKeys =
+    VehicleDetailsCacheKeys
+      .++(PrivateKeeperDetailsCacheKeys)
 }
--- test/views/changekeeper/BeforeYouStartIntegrationSpec.scala
+++ test/views/changekeeper/BeforeYouStartIntegrationSpec.scala
@@ -8,6 +8,9 @@ import helpers.webbrowser.TestHarness
 import pages.changekeeper.{VehicleLookupPage, BeforeYouStartPage}
 import pages.changekeeper.BeforeYouStartPage.startNow
 import pages.common.Feedback.EmailFeedbackLink
+import helpers.changekeeper.CookieFactoryForUISpecs
+import models.VehicleLookupFormModel.VehicleLookupFormModelCacheKey
+import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel.VehicleAndKeeperLookupDetailsCacheKey
 
 final class BeforeYouStartIntegrationSpec extends UiSpec with TestHarness {
   "go to page" should {
@@ -29,6 +32,17 @@ final class BeforeYouStartIntegrationSpec extends UiSpec with TestHarness {
     "not display the progress of the page when progressBar is set to false" taggedAs UiTag in new ProgressBarFalse {
       go to BeforeYouStartPage
       page.source.contains(progressStep(1)) should equal(false)
+    }
+
+    "clear all cookies stored in cache" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      CookieFactoryForUISpecs.vehicleLookupFormModel()
+      CookieFactoryForUISpecs.vehicleAndKeeperDetails()
+
+      go to BeforeYouStartPage
+
+      webDriver.manage().getCookieNamed(VehicleLookupFormModelCacheKey) should equal(null)
+      webDriver.manage().getCookieNamed(VehicleAndKeeperLookupDetailsCacheKey) should equal(null)
     }
   }
 
