da4d6e4a28c1996a26dfd174ea92cdf4b54c4464
Matthew Snaith <fault_line@hotmail.co.uk>
2014-11-12 15:13:00 +0000

    us1682 first commit with controller, view and models

---
+++ app/models/BusinessKeeperDetailsViewModel.scala
@@ -0,0 +1,7 @@
+package models
+
+import play.api.data.Form
+import uk.gov.dvla.vehicles.presentation.common
+import common.model.VehicleDetailsModel
+
+case class BusinessKeeperDetailsViewModel(form: Form[models.BusinessKeeperDetailsFormModel])
