fe2f02babee3673cb43501e28fde2a022f0f2020
Ian Stainer <ian.stainer@valtech.co.uk>
2014-11-24 09:40:07 +0000

    US1387 changed the version test

--- test/views/changekeeper/VersionIntegrationSpec.scala
+++ test/views/changekeeper/VersionIntegrationSpec.scala
@@ -1,4 +1,16 @@
 package views.changekeeper
-import uk.gov.dvla.vehicles.presentation.common.views
 
-class VersionIntegrationSpec extends views.VersionIntegrationSpec
+import helpers.UiSpec
+import helpers.webbrowser.{WebDriverFactory, TestHarness}
+import pages.ApplicationContext.applicationContext
+import scala.io.Source._
+
+class VersionIntegrationSpec extends UiSpec with TestHarness {
+  "Version endpoint" should {
+    "be declared and should include the build-details.txt from classpath" in new WebBrowser {
+      go.to(WebDriverFactory.testUrl + s"$applicationContext/version")
+      val t = fromInputStream(getClass.getResourceAsStream("/build-details.txt")).getLines().toList
+      page.source.lines.toSeq should contain allOf(t.head, t.tail.head, t.tail.tail.toSeq:_*)
+    }
+  }
+}
