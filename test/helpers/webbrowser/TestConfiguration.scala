1799b11ce55c19b2e951823af0615c9991863d57
Ian Stainer <ian.stainer@valtech.co.uk>
2014-12-02 09:59:09 +0000

    US1725 - removed commented code and replaced println with Logger

--- test/helpers/webbrowser/TestConfiguration.scala
+++ test/helpers/webbrowser/TestConfiguration.scala
@@ -1,16 +1,18 @@
 package helpers.webbrowser
 
+import play.api.Logger
+
 object TestConfiguration {
   private final val TestUrl = "test.url"
   private final val TestPort = "test.port"
   private final val DefaultTestPort = "9001"
 
   def testUrl: String = {
-    println(s"testUrl - Looking in for property $TestUrl in system props and environment vars...")
+    Logger.debug(s"testUrl - Looking in for property $TestUrl in system props and environment vars...")
     val sysOrEnvProp = sys.props.get(TestUrl)
       .orElse(sys.env.get(environmentVariableName(TestUrl)))
       .getOrElse(throw new RuntimeException(s"Error - cannot run tests. You need to configure property <$TestUrl>"))
-    println(s"testUrl - Found property $TestUrl in system or environment properties, value = $sysOrEnvProp")
+    Logger.debug(s"testUrl - Found property $TestUrl in system or environment properties, value = $sysOrEnvProp")
     sysOrEnvProp
   }
 
@@ -18,10 +20,10 @@ object TestConfiguration {
     val sysOrEnvProp = sys.props.get(TestPort)
       .orElse(sys.env.get(environmentVariableName(TestPort)))
       .getOrElse(DefaultTestPort)
-    println(s"testPort - $TestPort from system or environment properties or default value, value = $sysOrEnvProp")
+    Logger.debug(s"testPort - $TestPort from system or environment properties or default value, value = $sysOrEnvProp")
     val value = s"http://localhost:$sysOrEnvProp/"
     sys.props += ((TestUrl, value))
-    println(s"testPort - Set system property $TestUrl to value $value")
+    Logger.debug(s"testPort - Set system property $TestUrl to value $value")
     sysOrEnvProp.toInt
   }
 
