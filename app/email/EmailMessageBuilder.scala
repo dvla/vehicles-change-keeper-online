95f0aefe85bea8e5ef29c4aa9f605d872630716f
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2014-12-18 09:47:10 +0000

    US2000: styling of content

--- app/email/EmailMessageBuilder.scala
+++ app/email/EmailMessageBuilder.scala
@@ -20,19 +20,32 @@ object EmailMessageBuilder {
 
   private def buildHtml(vehicleDetails: VehicleAndKeeperDetailsModel, keeperDetails: NewKeeperDetailsViewModel): String =
     s"""
+        |<html>
+        |<head>
+        |</head>
+        |<style>
+        |p {
+        |  line-height: 200%;
+        |}
+        |</style>
+        |</head>
+        |<body>
         |<p><b>THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.</b></p>
         |<p>Dear Sir / Madam</p>
         |<p>We have been notified that you are now the registered keeper of this vehicle.
-        |<br /><br />
+        |<br />
         |You should receive your registration certificate (V5C) within 2 weeks.
-        |<br /><br />
-        |If you do not receive your V5C within this time or this information is incorrect then please contact DVLA on 0300 790 6802
-        |<br /><br />
+        |<br />
+        |If you do not receive your V5C within this time or this information is incorrect then please contact DVLA on
+        |<a href="callto:03007906802">0300 790 6802</a>
+        |<br />
         |For more information on the private sale of a vehicle go to <a href="https://www.gov.uk/dvla/xxxxxxxxx">
         |https://www.gov.uk/dvla/xxxxxxxxx</a>
         |</p>
         |
         |<p>Thank You</p>
+        |</body>
+        |</html>
       """.stripMargin
 
   private def buildText(vehicleDetails: VehicleAndKeeperDetailsModel, keeperDetails: NewKeeperDetailsViewModel): String =
