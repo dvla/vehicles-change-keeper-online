92a49f8e51be3ad14972419b837eea4607bc6c61
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2014-12-18 09:20:17 +0000

    US2000: Replace content of email

--- app/email/EmailMessageBuilder.scala
+++ app/email/EmailMessageBuilder.scala
@@ -20,24 +20,36 @@ object EmailMessageBuilder {
 
   private def buildHtml(vehicleDetails: VehicleAndKeeperDetailsModel, keeperDetails: NewKeeperDetailsViewModel): String =
     s"""
-        |<p>Dear Sir / Madam,</p>
-        |
+        |<p><b>THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.</b></p>
+        |<p>Dear Sir / Madam</p>
         |<p>We have been notified that you are now the registered keeper of this vehicle.
         |<br /><br />
         |You should receive your registration certificate (V5C) within 2 weeks.
         |<br /><br />
-        |If you do not receive your V5C within this time or this information is incorrect then please contact DVLA on 0300 790 6802</p>
+        |If you do not receive your V5C within this time or this information is incorrect then please contact DVLA on 0300 790 6802
+        |<br /><br />
+        |For more information on the private sale of a vehicle go to <a href="https://www.gov.uk/dvla/xxxxxxxxx">
+        |https://www.gov.uk/dvla/xxxxxxxxx</a>
+        |</p>
+        |
+        |<p>Thank You</p>
       """.stripMargin
 
   private def buildText(vehicleDetails: VehicleAndKeeperDetailsModel, keeperDetails: NewKeeperDetailsViewModel): String =
     s"""
-        |Dear Sir / Madam,
+        |THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.
         |
+        |
+        |Dear Sir / Madam
         |We have been notified that you are now the registered keeper of this vehicle.
         |
         |You should receive your registration certificate (V5C) within 2 weeks.
         |
         |If you do not receive your V5C within this time or this information is incorrect then please contact DVLA on 0300 790 6802
+        |
+        |For more information on the private sale of a vehicle go to https://www.gov.uk/dvla/xxxxxxxxx
+        |
+        |Thank You
       """.stripMargin
 
 }
