942b56fb36a648f5e6ae453a3eb7d9b9f7a14c55
Edmund <esr360@live.com>
2015-01-08 11:28:49 +0000

    fixing feedback form test

--- test/pages/common/Feedback.scala
+++ test/pages/common/Feedback.scala
@@ -2,7 +2,7 @@ package pages.common
 
 object Feedback {
   final val CommonSubject = "Private%20sale%20of%20a%20vehicle"
-  final val EmailFeedbackLink = "<a id=\"feedback\" target=\"_blank\" href=\"/feedback\">"
+  final val EmailFeedbackLink = s"""<a id="${views.common.Feedback.FeedbackId}" href="" onclick="window.open('${controllers.routes.FeedbackController.present()}', '_blank')">"""
 
   final val EmailHelpLink = "<a href=\"mailto:julie.riseley@dvla.gsi.gov.uk?" +
     s"""Subject=$CommonSubject%20help\">"""
