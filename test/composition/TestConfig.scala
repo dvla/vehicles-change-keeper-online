a8f37fee36ad7dcce4be2ef3106fa8843518c254
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-01-08 11:43:46 +0000

    US2177: replace vals with lazy vals

--- test/composition/TestConfig.scala
+++ test/composition/TestConfig.scala
@@ -9,31 +9,31 @@ import utils.helpers.Config
 object TestConfig extends Config {
 
   // Prototype message in html
-  override val isPrototypeBannerVisible: Boolean = true
+  override lazy val isPrototypeBannerVisible: Boolean = true
 
   // Google analytics
-  override val googleAnalyticsTrackingId: Option[String] = None
+  override lazy val googleAnalyticsTrackingId: Option[String] = None
 
   // Progress step indicator
-  override val isProgressBarEnabled: Boolean = false
+  override lazy val isProgressBarEnabled: Boolean = false
 
-  override val isHtml5ValidationEnabled: Boolean = false
+  override lazy val isHtml5ValidationEnabled: Boolean = false
 
-  override val startUrl: String = "/before-you-start"
+  override lazy val startUrl: String = "/before-you-start"
 
-  override val ordnanceSurveyUseUprn: Boolean = false
+  override lazy val ordnanceSurveyUseUprn: Boolean = false
 
   // opening and closing times
-  override val opening: Int = 1
-  override val closing: Int = 18
+  override lazy val opening: Int = 1
+  override lazy val closing: Int = 18
 
   // Web headers
-  override val applicationCode: String = ""
-  override val serviceTypeCode: String = ""
-  override val orgBusinessUnit: String = ""
+  override lazy val applicationCode: String = ""
+  override lazy val serviceTypeCode: String = ""
+  override lazy val orgBusinessUnit: String = ""
 
 
-  override val emailConfiguration: EmailConfiguration = EmailConfiguration(
+  override lazy val emailConfiguration: EmailConfiguration = EmailConfiguration(
     "",
     25,
     "",
