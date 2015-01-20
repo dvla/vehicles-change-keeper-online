b0be189d0c93f2fd2e713803a9c48cae04c9bd01
Chris Rudd <chris.rudd@valtech.co.uk>
2015-01-20 11:11:44 +0000

    US2244: Added web headers to DTOs.

--- app/utils/helpers/Config.scala
+++ app/utils/helpers/Config.scala
@@ -1,11 +1,13 @@
 package utils.helpers
 
+import uk.gov.dvla.vehicles.presentation.common.ConfigProperties._
 import uk.gov.dvla.vehicles.presentation.common.ConfigProperties.{getProperty, getOptionalProperty, getStringListProperty}
 import uk.gov.dvla.vehicles.presentation.common.services.SEND.{From, EmailConfiguration}
 
 class Config {
 
-  private lazy val notFound = "NOT FOUND"
+  private lazy val NotFound = "NOT FOUND"
+  private final val NotFoundLong = 9
 
   // Prototype message in html
   lazy val isPrototypeBannerVisible: Boolean = getProperty[Boolean]("prototype.disclaimer")
@@ -28,9 +30,11 @@ class Config {
   lazy val closing: Int = getProperty[Int]("closingTime")
 
   // Web headers
-  lazy val applicationCode: String = getOptionalProperty[String]("webHeader.applicationCode").getOrElse(notFound)
-  lazy val serviceTypeCode: String = getOptionalProperty[String]("webHeader.serviceTypeCode").getOrElse(notFound)
-  lazy val orgBusinessUnit: String = getOptionalProperty[String]("webHeader.orgBusinessUnit").getOrElse(notFound)
+  lazy val applicationCode: String = getOptionalProperty[String]("webHeader.applicationCode").getOrElse(NotFound)
+  lazy val serviceTypeCode: String = getOptionalProperty[String]("webHeader.serviceTypeCode").getOrElse(NotFound)
+  lazy val orgBusinessUnit: String = getOptionalProperty[String]("webHeader.orgBusinessUnit").getOrElse(NotFound)
+  lazy val channelCode: String = getOptionalProperty[String]("webHeader.channelCode").getOrElse(NotFound)
+  lazy val contactId: Long = getOptionalProperty[Long]("webHeader.contactId").getOrElse(NotFoundLong)
 
 
   lazy val emailConfiguration: EmailConfiguration = EmailConfiguration(
