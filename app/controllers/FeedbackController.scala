4b30c630461ee8c3accf3d6a410134a2ce0c1a6a
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-02-06 14:30:32 +0000

    US2340: Add email and name to feedback page

--- app/controllers/FeedbackController.scala
+++ app/controllers/FeedbackController.scala
@@ -1,12 +1,15 @@
 package controllers
 
 import com.google.inject.Inject
-import play.api.data.Form
+import play.api.data.{FormError, Form}
 import play.api.i18n.Messages
 import play.api.mvc._
+import uk.gov.dvla.vehicles.presentation.common
+import common.views.helpers.FormExtensions.formBinding
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.controllers.FeedbackBase
 import uk.gov.dvla.vehicles.presentation.common.model.FeedbackForm
+import uk.gov.dvla.vehicles.presentation.common.model.FeedbackForm.Form.{emailMapping, nameMapping, feedback}
 import utils.helpers.Config
 
 class FeedbackController @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
@@ -18,24 +21,33 @@ class FeedbackController @Inject()()(implicit clientSideSessionFactory: ClientSi
     FeedbackForm.Form.Mapping
   )
 
-  private final val CookieErrorMessage = "Did not find VehicleDetailsModel cookie. Now redirecting to SetUpTradeDetails."
-
   implicit val controls: Map[String, Call] = Map(
   "submit" -> controllers.routes.FeedbackController.submit()
   )
 
   def present() = Action { implicit request =>
-    Ok(views.html.changekeeper.feedback( form.fill(FeedbackForm(""))) )
+    Ok(views.html.changekeeper.feedback(form))
   }
 
   def submit: Action[AnyContent] = Action { implicit request =>
     form.bindFromRequest.fold (
-      invalidForm => BadRequest(views.html.changekeeper.feedback(invalidForm)),
+      invalidForm =>
+        BadRequest(views.html.changekeeper.feedback(formWithReplacedErrors(invalidForm))),
         validForm => {
-          sendFeedback(validForm.feedback, Messages("common_feedback.subject"))
+          sendFeedback(validForm, Messages("common_feedback.subject"))
           Ok(views.html.changekeeper.feedbackSuccess())
         }
     )
+  }
+
+  private def formWithReplacedErrors(form: Form[FeedbackForm]) = {
+    form.replaceError(
+      feedback, FormError(key = feedback,message = "error.feedback", args = Seq.empty)
+    ).replaceError(
+        nameMapping, FormError(key = nameMapping, message = "error.feedbackName", args = Seq.empty)
+      ).replaceError(
+        emailMapping, FormError(key = emailMapping, message = "error.email", args = Seq.empty)
+      ).distinctErrors
   }
 
 }
