e41597756290de803f52ba32399548ec7be61ab8
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-03-10 14:50:21 +0000

    US2673: clean up email template

--- app/email/EmailMessageBuilder.scala
+++ app/email/EmailMessageBuilder.scala
@@ -32,19 +32,21 @@ object EmailMessageBuilder {
         |<body>
         |<p><b>THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.</b></p>
         |<p>Dear Sir / Madam</p>
-        |<p>We have been notified that you are now the registered keeper of this vehicle.
-        |<br />
+        |<p>We have been notified that you are now the registered keeper of this vehicle.</p>
+        |
         |<p>
         |Since 1st October 2014, the vehicle tax can no longer be transferred as part of the sale.
         |This is because the seller will automatically receive a refund of any remaining tax.
         |Buyers must now get the vehicle taxed before it can be used.
         |</p>
+        |
         |<p>
         |You should receive your registration certificate (V5C) within 2 weeks.
         |If you do not receive your V5C within this time or this information is incorrect then please contact DVLA on
         |<a href="callto:03007906802">0300 790 6802</a>
         |</p>
-        |<br />
+        |
+        |<p>
         |For more information on driving and transport go to <a href="https://www.gov.uk/browse/driving">
         |https://www.gov.uk/browse/driving</a>
         |</p>
