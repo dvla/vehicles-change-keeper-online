f411de5892dcd44afb749c26a2b8d0e71892d1fa
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-04-24 00:34:39 +0100

    US2860 use a single require.js configuration file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,22 +1,7 @@
-require.config({
-    paths: {
-        'jquery' : '../lib/jquery/jquery',
-        'jquery-migrate': '../lib/jquery-migrate/jquery-migrate',
-        'header-footer-only': '../lib/vehicles-presentation-common/javascripts/header-footer-only',
-        'global-helpers': '../lib/vehicles-presentation-common/javascripts/global-helpers',
-        'page-init': '../lib/vehicles-presentation-common/javascripts/page-init',
-        'change-keeper-page-init': 'change-keeper-page-init'
-    },
-    // Make jquery-migrate depend on the loading of jquery
-    "shim": {
-        'jquery-migrate': ['jquery']
-    }
-});
-
-require(["change-keeper-page-init"], function(changeKeeperPageInit) {
-    $(function() {
-        changeKeeperPageInit.init();
-
-
+require(['config'], function() {
+    require(["change-keeper-page-init"], function(changeKeeperPageInit) {
+        $(function() {
+            changeKeeperPageInit.init();
+        });
     });
 });
