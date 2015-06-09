1abe039053d1200a8ef02ca9a9d139530b1264c0
Luchesar Cekov <luchesar@gmail.com>
2015-06-09 10:13:24 +0100

    DE544 fix a javascript syntax error

--- app/assets/javascripts/change-keeper-page-init.js
+++ app/assets/javascripts/change-keeper-page-init.js
@@ -29,10 +29,10 @@ define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pag
 
     var trackPrivateBusiness = function() {
         if ($("#vehicleSoldTo_Private").is(':checked')) {
-            _gaq.push(['_trackEvent', "track_path", "individual", '', value]);
+            _gaq.push(['_trackEvent', "track_path", "individual", '', 1]);
         }
         if ($("#vehicleSoldTo_Business").is(':checked')) {
-            _gaq.push(['_trackEvent', "track_path", "business", '', value]);
+            _gaq.push(['_trackEvent', "track_path", "business", '', 1]);
         }
 
     };
