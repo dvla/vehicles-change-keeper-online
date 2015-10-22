dba9ca44a3c9311fea42fb32b1f0b167f48999bf
Ian Stainer <ian.stainer@valtech.co.uk>
2015-10-22 18:19:07 +0100

    US3949 Added a test for the application root controller

---
+++ test/controllers/ApplicationUnitSpec.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import composition.WithApplication
+import helpers.UnitSpec
+import org.mockito.Mockito.when
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{defaultAwaitTimeout, redirectLocation}
+import uk.gov.dvla.vehicles.presentation.common.ConfigProperties.{getProperty, stringProp}
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import utils.helpers.Config
+
+class ApplicationUnitSpec extends UnitSpec {
+  lazy val applicationContext = getProperty[String]("application.context")
+
+  "index" should {
+    "redirect the user to the start url" in new WithApplication {
+      implicit val config = configWithStartUrl("/testStart")
+      implicit val clientSideSessionFactory = injector.getInstance(classOf[ClientSideSessionFactory])
+      val result = new Application().index(FakeRequest())
+      redirectLocation(result) should equal(Some("/testStart"))
+    }
+
+    "redirect the user to the start url when the start url does not have a starting slash" in new WithApplication {
+      implicit val config = configWithStartUrl("testStart")
+      implicit val clientSideSessionFactory = injector.getInstance(classOf[ClientSideSessionFactory])
+      val result = new Application().index(FakeRequest())
+      redirectLocation(result) should equal(Some("testStart"))
+    }
+
+    "redirect the user to the start url when the start url has application context" in new WithApplication {
+      implicit val config = configWithStartUrl("/testContext/testStart")
+      implicit val clientSideSessionFactory = injector.getInstance(classOf[ClientSideSessionFactory])
+      val result = new Application().index(FakeRequest())
+      redirectLocation(result) should equal(Some("/testContext/testStart"))
+    }
+  }
+
+  private def configWithStartUrl(startUrl: String): Config = {
+    val config = mock[Config]
+    when(config.startUrl).thenReturn(startUrl)
+    config
+  }
+}
