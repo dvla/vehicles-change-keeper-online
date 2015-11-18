1c280fbf4f8aabbbc3aa5120b3025dbcea026600
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2015-11-18 13:19:04 +0000

    US3977 Create a CookiePolicy controller in vehicles-presentation-common

---
+++ app/controllers/CookiePolicy.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.mvc.{Action, Controller}
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import utils.helpers.Config
+
+class CookiePolicy@Inject()(implicit clientSideSessionFactory: ClientSideSessionFactory,
+                            config: Config)
+                            extends uk.gov.dvla.vehicles.presentation.common.controllers.CookiePolicy {
+
+  def present = Action { implicit request =>
+    Ok(views.html.changekeeper.cookie_policy(cookies))
+  }
+}
