72f2b0a9ceec1fd1b7924e20829c90da5b0d7389
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2015-12-08 08:08:09 +0000

    US4052 PR address picker add three alphas constraint
    Remove unused model. It is in v-p-c.

--- app/models/NewKeeperChooseYourAddressViewModel.scala
+++
@@ -1,8 +0,0 @@
-package models
-
-import play.api.data.Form
-import uk.gov.dvla.vehicles.presentation.common.model.NewKeeperChooseYourAddressFormModel
-import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel
-
-case class NewKeeperChooseYourAddressViewModel(form: Form[NewKeeperChooseYourAddressFormModel],
-                                               vehicleDetails: VehicleAndKeeperDetailsModel)
