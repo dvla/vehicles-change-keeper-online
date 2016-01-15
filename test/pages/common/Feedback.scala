15e2869d2838429e4eb801261e423ebf5f014813
Oliver Jenkins <github@oliverjenkins.com>
2016-01-15 13:44:59 +0000

    US4014 Redundant Feedback object has been removed

--- test/pages/common/Feedback.scala
+++ test/pages/common/Feedback.scala
@@ -2,7 +2,7 @@ package pages.common
 
 object Feedback {
   final val CommonSubject = "Private%20sale%20of%20a%20vehicle"
-  final val EmailFeedbackLink = s"""<a id="${views.common.Feedback.FeedbackId}"""" +
+  final val EmailFeedbackLink = s"""<a id="${views.common.ProtoType.FeedbackId}"""" +
     s""" href="${controllers.routes.FeedbackController.present()}" target="_blank">"""
 
   final val EmailHelpLink = "<a href=\"mailto:julie.riseley@dvla.gsi.gov.uk?" +
