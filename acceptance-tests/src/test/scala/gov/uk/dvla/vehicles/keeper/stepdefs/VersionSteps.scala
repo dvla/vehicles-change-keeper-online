e9622e4aba6c0bb188e92fda3a4cd591a17059d4
Ian Stainer <ian.stainer@valtech.co.uk>
2016-09-23 10:45:46 +0100

    VMPRCI-4513 rolled back temp fix

--- acceptance-tests/src/test/scala/gov/uk/dvla/vehicles/keeper/stepdefs/VersionSteps.scala
+++ acceptance-tests/src/test/scala/gov/uk/dvla/vehicles/keeper/stepdefs/VersionSteps.scala
@@ -7,9 +7,7 @@ import org.apache.http.conn.ssl.{TrustStrategy, SSLContexts}
 import org.apache.http.impl.client.HttpClientBuilder
 import org.openqa.selenium.WebDriver
 import scala.io.Source.fromInputStream
-import uk.gov.dvla.vehicles.presentation.common.helpers
-import helpers.webbrowser.{WebBrowserDriver, WebDriverFactory}
-import org.scalatest.selenium.WebBrowser.{TextField, textField, TelField, telField, RadioButton, radioButton, click, go, find, id, Element}
+import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.{WebBrowserDriver, WebDriverFactory}
 
 class VersionSteps(webBrowserDriver: WebBrowserDriver) extends gov.uk.dvla.vehicles.keeper.helpers.AcceptanceTestHelper {
 
@@ -39,8 +37,9 @@ class VersionSteps(webBrowserDriver: WebBrowserDriver) extends gov.uk.dvla.vehic
 
   @Then("^The user should be able to see version and runtime information for the microservices$")
   def the_user_should_be_able_to_see_version_and_runtime_information_for_the_microservices() {
-//    versionString should include("vehicle-and-keeper-lookup")
-//    versionString should include("os-address-lookup")
-//    versionString should include("vehicles-acquire-fulfil")
+    versionString should include("email-service")
+    versionString should include("os-address-lookup")
+    versionString should include("vehicle-and-keeper-lookup")
+    versionString should include("vehicles-acquire-fulfil")
   }
 }
